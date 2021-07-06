5eb6632773b359bdede6bb6fe88dc22b6bd3d52a
J-Whisler <jwhisler521@gmail.com>
2021-07-05 21:32:04 -0400

    main game functionality created and displayed to UI

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,19 +1,155 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import GameInfo from "./GameInfo";
 
 const Game = () => {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [userScore, setUserScore] = useState(0);
+  const [result, setResult] = useState(null);
+
+  const generateComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    const randomChoice = choices[randomNumber];
+    setComputerChoice(randomChoice);
+  };
+
+  useEffect(() => {
+    switch (userChoice + computerChoice) {
+      case "ScissorsPaper":
+      case "RockScissors":
+      case "PaperRock":
+        setResult("You Win!");
+        setUserScore(userScore + 1);
+        break;
+      case "PaperScissors":
+      case "ScissorsRock":
+      case "RockPaper":
+        setResult("You Lose!");
+        setUserScore(userScore - 1);
+        break;
+      case "RockRock":
+      case "PaperPaper":
+      case "ScissorsScissors":
+        setResult("Tie!");
+        setUserScore(userScore);
+        break;
+    }
+  }, [computerChoice, userChoice]);
+
   return (
     <>
-      <GameInfo />
-      <GameContainer>Game</GameContainer>
+      <GameInfo
+        userScore={userScore}
+        setUserScore={setUserScore}
+        userChoice={userChoice}
+        computerChoice={computerChoice}
+        result={result}
+      />
+      <GameContainer>
+        <h1>Rock, Paper, Scissors</h1>
+
+        <div className="game-board">
+          <div className="game-board-top">
+            <h2>
+              <i
+                className={
+                  userChoice === "Rock"
+                    ? "fas fa-hand-rock active"
+                    : "fas fa-hand-rock"
+                }
+                onClick={() => {
+                  setUserChoice("Rock");
+                  generateComputerChoice();
+                }}
+              ></i>
+            </h2>
+          </div>
+          <div className="game-board-bottom">
+            <h2>
+              <i
+                className={
+                  userChoice === "Paper"
+                    ? "fas fa-hand-paper active"
+                    : "fas fa-hand-paper"
+                }
+                onClick={() => setUserChoice("Paper")}
+              ></i>
+            </h2>
+
+            <h2>
+              <i
+                className={
+                  userChoice === "Scissors"
+                    ? "fas fa-hand-scissors active"
+                    : "fas fa-hand-scissors"
+                }
+                onClick={() => setUserChoice("Scissors")}
+              ></i>
+            </h2>
+          </div>
+          {/* <div className="button-container">
+            {choices.map((choice, index) => (
+              <button key={index}>{choice}</button>
+            ))}
+          </div> */}
+        </div>
+      </GameContainer>
     </>
   );
 };
 
 const GameContainer = styled.div`
-  border: 2px solid red;
-  height: 90vh;
+  /* border: 2px solid red; */
+  height: 85.1vh;
+
+  text-align: center;
+  background: #050a30;
+  color: #7ec8e3;
+  h1 {
+    font-size: 3rem;
+  }
+  .game-board {
+    /* border: 2px solid yellow; */
+    height: 77vh;
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+
+    i {
+      color: #050a30;
+      cursor: pointer;
+      background-color: #7ec8e3;
+      font-size: 4rem;
+      padding: 3rem;
+      border-radius: 50%;
+
+      border: 0.8rem solid #050a30;
+      transition: all 0.7s ease-in-out;
+      &.active {
+        border: 0.8rem solid #fad46d;
+        box-shadow: 0px 0px 30px #f7db91;
+      }
+    }
+
+    .game-board-top {
+      /* border: 2px solid blue; */
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .game-board-bottom {
+      /* border: 2px solid green; */
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      h4 {
+        font-size: 4rem;
+        color: #fad46d;
+        /* text-shadow: 0px 0px 3px #fad46d; */
+      }
+    }
+  }
 `;
 
 export default Game;
--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -1,13 +1,124 @@
 import { useState } from "react";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 
-const GameInfo = () => {
-  return <GameInfoContainer>Game Info</GameInfoContainer>;
+// const userChoiceAnim = {
+//   initial: {
+//     opacity: 0,
+//   },
+//   animate: {
+//     opacity: 1,
+//     transition: {
+//       duration: 1,
+//       delay: 0.5,
+//     },
+//   },
+//   exit: {
+//     opacity: 0,
+//   },
+// };
+
+const GameInfo = ({
+  userScore,
+  setUserScore,
+  userChoice,
+  computerChoice,
+  result,
+}) => {
+  //   const getUserChoice = () => {
+  //     if (userChoice === "rock") {
+  //       return "Rock";
+  //     } else if (userChoice === "paper") {
+  //       return "Paper";
+  //     } else if (userChoice === "scissors") {
+  //       return "Scissors";
+  //     } else {
+  //       return;
+  //     }
+  //   };
+  return (
+    <GameInfoContainer>
+      <div className="choices-container">
+        <h3>
+          Your Choice: <br />
+          <AnimatePresence>
+            <em
+            //   variants={userChoiceAnim}
+            //   initial="initial"
+            //   animate="animate"
+            //   exit="exit"
+            >
+              {userChoice}
+            </em>
+          </AnimatePresence>
+        </h3>
+        <h3>
+          Computer Choice: <br /> <em>{computerChoice}</em>
+        </h3>
+      </div>
+      <div className="result-container">
+        <h2>{result}</h2>
+      </div>
+      <div className="score-container">
+        <h3>Your Score: {userScore}</h3>
+        <button onClick={() => setUserScore(0)}>Reset</button>
+      </div>
+    </GameInfoContainer>
+  );
 };
 
 const GameInfoContainer = styled.div`
-  border: 2px solid blue;
-  height: 10vh;
+  /* border: 2px solid blue; */
+  height: 15vh;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  background: #050a30;
+  padding: 0 2rem;
+  .choices-container {
+    /* border: 2px solid green; */
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h3 {
+      color: #fad46d;
+      font-size: 1.5rem;
+      em {
+        font-size: 2rem;
+        transition: all 0.5s ease-in-out;
+      }
+    }
+  }
+  .result-container {
+    /* border: 2px solid blue; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    h2 {
+      color: #fad46d;
+      font-size: 2.5rem;
+    }
+  }
+  .score-container {
+    /* border: 2px solid red; */
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    h3 {
+      color: #fad46d;
+      font-size: 1.5rem;
+    }
+    button {
+      width: 7rem;
+      padding: 0.5rem;
+      color: #000c66;
+      font-size: 1.2rem;
+      letter-spacing: 0.3rem;
+      background-color: #7ec8e3;
+      border: 2px solid #000c66;
+      border-radius: 1rem;
+      cursor: pointer;
+    }
+  }
 `;
 
 export default GameInfo;
